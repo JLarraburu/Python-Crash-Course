96a697383da53387be6dfe0a7c44377907c4becc
Jon Larraburu <jon.larraburu@gmail.com>
2020-02-20 23:49:22 +0000

    Starting on some classes

---
+++ Part 1/Working with Classes and Instances.py
@@ -0,0 +1,63 @@
+class Car():
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        #   Setting a default value for an attribute
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name"""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    #   Modifying an attribute method through a method
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value
+        Reject the change if it attempts to roll the odometer back
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll bak an odometer!")
+    
+    #   Incrementing an attribute's value through a method
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading"""
+        self.odometer_reading += miles
+
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
+#   Modifying an attribute's value directly
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
+my_new_car.update_odometer(63)
+my_new_car.read_odometer()
+
+my_used_car = Car('subaru', 'outback', 2013)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(230000)
+my_used_car.read_odometer()
+
+#   Inheritance
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__(make, model, year)
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
--- Part 1/dog.py
+++ Part 1/dog.py
@@ -18,3 +18,6 @@ my_dog = Dog('Bluebell', 5)
 
 print("My dog's name is " + my_dog.name.title() + ".")
 print("My dog is " + str(my_dog.age) + " years old.")
+
+my_dog.sit()
+my_dog.roll_over()
